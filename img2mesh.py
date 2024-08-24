412021b52647a1c36a4a84225f34583b5a8022fb
Ilya Razmanov <ilyarazmanov@gmail.com>
2024-08-24 17:06:13 +0300

    main shell updated

--- img2mesh.py
+++ img2mesh.py
@@ -9,7 +9,9 @@ Created by: Ilya Razmanov (mailto:ilyarazmanov@gmail.com)
             aka Ilyich the Toad (mailto:amphisoft@gmail.com)
 History:
 
-1.0.0.0 Initial production release.
+1.0.0.0     Initial production release.
+1.8.24.1    DXF export added, POV export changed, multiple changes everywhere lead to whole product update.
+            Versioning changed to MAINVERSION.MONTH since Jan 2024.DAY.subversion
 
         Main site:
         https://dnyarri.github.io
@@ -24,7 +26,7 @@ __author__ = "Ilya Razmanov"
 __copyright__ = "(c) 2024 Ilya Razmanov"
 __credits__ = "Ilya Razmanov"
 __license__ = "unlicense"
-__version__ = "1.0.0.0"
+__version__ = "1.8.24.1"
 __maintainer__ = "Ilya Razmanov"
 __email__ = "ilyarazmanov@gmail.com"
 __status__ = "Production"
@@ -37,6 +39,7 @@ from pathlib import Path
 from img2pov import img2pov
 from img2obj import img2obj
 from img2stl import img2stl
+from img2dxf import img2dxf
 
 # ACHTUNG! User break definition below. Take care.
 DyeDye = False  # Variable for breaking program anywhere upon conditions
@@ -67,8 +70,8 @@ stopper.title('IMG2MESH')
 if useicon:
     stopper.iconbitmap(iconname)
 stopper.geometry('+200+100')
-stopper.minsize(300, 300)
-stopper.maxsize(400, 400)
+stopper.minsize(300, 360)
+stopper.maxsize(500, 500)
 
 preved01 = Label(stopper, text = 'img2mesh', font=("arial", 36), padx=16, pady=10, justify='center')
 preved01.pack(side=TOP, fill=X)
@@ -76,22 +79,22 @@ preved01.pack(side=TOP, fill=X)
 preved02 = Label(stopper, text = 'PNG height fields to 3D mesh converter', font=("arial", 12), padx=16, pady=10, justify='center')
 preved02.pack(side=TOP, fill=X)
 
-butt01 = Button(stopper, text='PNG to POV...', font=('arial', 16), cursor='hand2', justify='center', command=img2pov
-)
+butt01 = Button(stopper, text='PNG to POV...', font=('arial', 16), cursor='hand2', justify='center', command=img2pov)
 butt01.pack(side=TOP, padx=4, pady=2, fill=X)
 
-butt02 = Button(stopper, text='PNG to OBJ...', font=('arial', 16), cursor='hand2', justify='center', command=img2obj
-)
+butt02 = Button(stopper, text='PNG to OBJ...', font=('arial', 16), cursor='hand2', justify='center', command=img2obj)
 butt02.pack(side=TOP, padx=4, pady=2, fill=X)
 
-butt03 = Button(stopper, text='PNG to STL...', font=('arial', 16), cursor='hand2', justify='center', command=img2stl
-)
+butt03 = Button(stopper, text='PNG to STL...', font=('arial', 16), cursor='hand2', justify='center', command=img2stl)
 butt03.pack(side=TOP, padx=4, pady=2, fill=X)
 
-butt04 = Button(
+butt04 = Button(stopper, text='PNG to DXF...', font=('arial', 16), cursor='hand2', justify='center', command=img2dxf)
+butt04.pack(side=TOP, padx=4, pady=2, fill=X)
+
+butt09 = Button(
     stopper, text='Exit', font=('arial', 16), cursor='hand2', justify='center', command=DyeDyeMyDarling
 )
-butt04.pack(side=BOTTOM, padx=4, pady=2, fill=X)
+butt09.pack(side=BOTTOM, padx=4, pady=(8, 2), fill=X)
 
 stopper.mainloop()
 
