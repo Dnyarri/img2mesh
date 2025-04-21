cee6337e55cf684a47e2b4b56d240933888d802b
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-04-21 16:59:26 +0300

    Menu-based GUI

--- img2mesh.py
+++ img2mesh.py
@@ -4,16 +4,19 @@
 IMG2MESH - Program for conversion of image heightfield to triangle 3D-mesh in different formats
 ------------------------------------------------------------------------------------------------
 
-Created by: 
+Created by:
 `Ilya Razmanov <mailto:ilyarazmanov@gmail.com>`_ aka
 `Ilyich the Toad <mailto:amphisoft@gmail.com>`_.
 
 History
 --------
 
-2.13.13.2   Previous version of img2mesh GUI replaced with completely new joint (PyPNG, PyPNM) ➔ (list2pov, list2obj, list2stl, list2dxf) program with the same name.
+2.13.13.2   Previous version of img2mesh GUI replaced with completely new joint
+(PyPNG, PyPNM) ➔ (list2pov, list2obj, list2stl, list2dxf) program with the same name.
 
 3.14.16.1   list2mesh components upgraded to version 3 with improved geometry.
+
+3.16.20.20  New minimalistic menu-based GUI.
 
 ----
 Main site: `The Toad's Slimy Mudhole <https://dnyarri.github.io>`_
@@ -27,28 +30,32 @@ __author__ = 'Ilya Razmanov'
 __copyright__ = '(c) 2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '3.16.4.8'
+__version__ = '3.16.20.20'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
 
 from pathlib import Path
-from tkinter import Button, Frame, Label, PhotoImage, Tk, filedialog
+from tkinter import Button, Frame, Label, Menu, PhotoImage, Tk, filedialog
 
 from list2mesh import list2dxf, list2obj, list2pov, list2stl
 from pypng import pnglpng
 from pypnm import pnmlpnm
 
 
-def DisMiss():
+def DisMiss(event=None):
     """Kill dialog and continue"""
-
     sortir.destroy()
+
+
+def ShowMenu(event):
+    """Pop menu up (or sort of drop it down)"""
+    menu01.post(event.x_root, event.y_root)
 
 
 def UINormal():
     """Normal UI state, buttons enabled"""
-    for widget in frame_left.winfo_children():
+    for widget in frame_img.winfo_children():
         if widget.winfo_class() in ('Label', 'Button'):
             widget.config(state='normal')
     info_string.config(text=info_normal['txt'], foreground=info_normal['fg'], background=info_normal['bg'])
@@ -56,7 +63,7 @@ def UINormal():
 
 def UIBusy():
     """Busy UI state, buttons disabled"""
-    for widget in frame_left.winfo_children():
+    for widget in frame_img.winfo_children():
         if widget.winfo_class() in ('Label', 'Button'):
             widget.config(state='disabled')
     info_string.config(text=info_busy['txt'], foreground=info_busy['fg'], background=info_busy['bg'])
@@ -66,17 +73,20 @@ def UIBusy():
 def GetSource(event=None):
     """Opening source image and redefining other controls state"""
 
-    global zoom_factor, zoom_do, zoom_show, sourcefilename, preview, preview_data
-    global X, Y, Z, maxcolors, image3D
+    global zoom_factor, zoom_do, zoom_show, preview, preview_data
+    global X, Y, Z, maxcolors, image3D, sourcefilename
     zoom_factor = 0
+
     sourcefilename = filedialog.askopenfilename(title='Open image file', filetypes=[('Supported formats', '.png .ppm .pgm .pbm'), ('PNG', '.png'), ('PNM', '.ppm .pgm .pbm')])
     if sourcefilename == '':
-        return None
+        return
+
+    UIBusy()
 
     """ ┌────────────────────────────────────────┐
         │ Loading file, converting data to list. │
         │  NOTE: maxcolors, image3D are GLOBALS! │
-        │  This is required for preview to work. │
+        │  They are used during export!          │
         └────────────────────────────────────────┘ """
 
     if Path(sourcefilename).suffix == '.png':
@@ -92,13 +102,14 @@ def GetSource(event=None):
 
     """ ┌─────────────────────────────────────────────────────────────────────────┐
         │ Converting list to bytes of PPM-like structure "preview_data" in memory │
-        └─────────────────────────────────────────────────────────────────────────┘ """
+        └────────────────────────────────────────────────────────────────────────-┘ """
     preview_data = pnmlpnm.list2bin(image3D, maxcolors, show_chessboard=True)
 
     """ ┌────────────────────────────────────────────────┐
         │ Now showing "preview_data" bytes using Tkinter │
         └────────────────────────────────────────────────┘ """
     preview = PhotoImage(data=preview_data)
+
     zoom_show = {  # What to show below preview
         -4: 'Zoom 1:5',
         -3: 'Zoom 1:4',
@@ -110,7 +121,7 @@ def GetSource(event=None):
         3: 'Zoom 4:1',
         4: 'Zoom 5:1',
     }
-    zoom_do = {  # What to do to preview
+    zoom_do = {  # What to do to preview; "zoom" zooms in, "subsample" zooms out
         -4: preview.subsample(5, 5),
         -3: preview.subsample(4, 4),
         -2: preview.subsample(3, 3),
@@ -122,23 +133,26 @@ def GetSource(event=None):
         4: preview.zoom(5, 5),
     }
 
-    preview = zoom_do[zoom_factor]  # "zoom" zooms in, "subsample" zooms out
-    zanyato.config(text='Source', font=('helvetica', 10), image=preview, compound='top', state='normal')
+    preview = zoom_do[zoom_factor]
+    zanyato.config(image=preview, compound='none', justify='center', background=zanyato.master['background'], relief='flat', borderwidth=1)
     # binding zoom on preview click
-    zanyato.bind('<Button-1>', zoomIn)  # left
-    zanyato.bind('<Alt-Button-1>', zoomOut)  # left
-    zanyato.bind('<Button-2>', zoomOut)  # middle
-    zanyato.bind('<Button-3>', zoomOut)  # right
+    zanyato.bind('<Control-Button-1>', zoomIn)  # Ctrl + left click
+    zanyato.bind('<Double-Control-Button-1>', zoomIn)  # Ctrl + left click too fast
+    zanyato.bind('<Alt-Button-1>', zoomOut)  # Alt + left click
+    zanyato.bind('<Double-Alt-Button-1>', zoomOut)  # Alt + left click too fast
+    sortir.bind_all('<MouseWheel>', zoomWheel)  # Wheel
     # enabling zoom buttons
     butt_plus.config(state='normal', cursor='hand2')
     butt_minus.config(state='normal', cursor='hand2')
     # updating zoom label display
     label_zoom.config(text=zoom_show[zoom_factor])
-    # updating "Export..." status
-    butt02.config(state='normal', cursor='hand2')
-    butt03.config(state='normal', cursor='hand2')
-    butt04.config(state='normal', cursor='hand2')
-    butt05.config(state='normal', cursor='hand2')
+    # enabling "Save as..."
+    menu01.entryconfig('Export POV-Ray...', state='normal')  # Instead of name numbers from 0 may be used
+    menu01.entryconfig('Export OBJ...', state='normal')
+    menu01.entryconfig('Export DXF...', state='normal')
+    menu01.entryconfig('Export STL...', state='normal')
+
+    UINormal()
 
 
 def SaveAsPOV():
@@ -247,7 +261,7 @@ def zoomIn(event=None):
     zoom_factor = min(zoom_factor + 1, 4)  # max zoom 5
     preview = PhotoImage(data=preview_data)
     preview = zoom_do[zoom_factor]
-    zanyato.config(image=preview, compound='top')
+    zanyato.config(image=preview, compound='none')
     # updating zoom factor display
     label_zoom.config(text=zoom_show[zoom_factor])
     # reenabling +/- buttons
@@ -263,7 +277,7 @@ def zoomOut(event=None):
     zoom_factor = max(zoom_factor - 1, -4)  # min zoom 1/5
     preview = PhotoImage(data=preview_data)
     preview = zoom_do[zoom_factor]
-    zanyato.config(image=preview, compound='top')
+    zanyato.config(image=preview, compound='none')
     # updating zoom factor display
     label_zoom.config(text=zoom_show[zoom_factor])
     # reenabling +/- buttons
@@ -274,67 +288,80 @@ def zoomOut(event=None):
         butt_minus.config(state='normal', cursor='hand2')
 
 
+def zoomWheel(event):
+    if event.delta < 0:
+        zoomOut()
+    if event.delta > 0:
+        zoomIn()
+
+
 """ ╔═══════════╗
     ║ Main body ║
     ╚═══════════╝ """
 
+# Starting values
+zoom_factor = 0
+sourcefilename = X = Y = Z = maxcolors = None
+
 sortir = Tk()
 
-zoom_factor = 0
+sortir.title('img2mesh')
+sortir.geometry('+200+100')
+sortir.minsize(128, 128)
 
-iconpath = Path(__file__).resolve().parent / 'vaba.ico'
-if iconpath.exists():
-    sortir.iconbitmap(str(iconpath))
+icon_path = Path(__file__).resolve().parent / 'vaba.ico'
+if icon_path.exists():
+    sortir.iconbitmap(str(icon_path))
 else:
     sortir.iconphoto(True, PhotoImage(data=b'P6\n2 2\n255\n\xff\x00\x00\xff\xff\x00\x00\x00\xff\x00\xff\x00'))
 
-sortir.title(f'img2mesh v. {__version__}')
-sortir.geometry('+200+100')
-sortir.minsize(300, 320)
-
 # Info statuses dictionaries
-info_normal = {'txt': 'Bitmap height field to 3D mesh converter', 'fg': 'grey', 'bg': 'light grey'}
+info_normal = {'txt': f'img2mesh {__version__}', 'fg': 'grey', 'bg': 'grey90'}
 info_busy = {'txt': 'BUSY, PLEASE WAIT', 'fg': 'red', 'bg': 'yellow'}
 
-info_string = Label(sortir, text=info_normal['txt'], font=('courier', 8), foreground=info_normal['fg'], background=info_normal['bg'], relief='groove')
-info_string.pack(side='bottom', padx=0, pady=1, fill='both')
+info_string = Label(sortir, text=info_normal['txt'], font=('courier', 7), foreground=info_normal['fg'], background=info_normal['bg'], relief='groove')
+info_string.pack(side='bottom', padx=0, pady=(2, 0), fill='both')
 
-frame_left = Frame(sortir, borderwidth=2, relief='groove')
-frame_left.pack(side='left', anchor='nw')
-frame_right = Frame(sortir, borderwidth=2, relief='groove')
-frame_right.pack(side='right', anchor='nw')
+menu01 = Menu(sortir, tearoff=False)  # Drop-down
+menu01.add_command(label='Open...', state='normal', accelerator='Ctrl+O', command=GetSource)
+menu01.add_separator()
+menu01.add_command(label='Export POV-Ray...', state='disabled', command=SaveAsPOV)
+menu01.add_command(label='Export OBJ...', state='disabled', command=SaveAsOBJ)
+menu01.add_command(label='Export DXF...', state='disabled', command=SaveAsDXF)
+menu01.add_command(label='Export STL...', state='disabled', command=SaveAsSTL)
+menu01.add_separator()
+menu01.add_command(label='Exit', state='normal', accelerator='Ctrl+Q', command=DisMiss)
 
-butt01 = Button(frame_left, text='Open image...'.center(30, ' '), font=('helvetica', 14), cursor='hand2', justify='center', command=GetSource)
-butt01.pack(side='top', padx=4, pady=[4, 12], fill='both')
+sortir.bind('<Button-3>', ShowMenu)
+sortir.bind_all('<Alt-f>', ShowMenu)
+sortir.bind_all('<Control-o>', GetSource)
+sortir.bind_all('<Control-q>', DisMiss)
 
-butt02 = Button(frame_left, text='Export POV...', font=('helvetica', 14), cursor='arrow', justify='center', state='disabled', command=SaveAsPOV)
-butt02.pack(side='top', padx=4, pady=2, fill='both')
+frame_img = Frame(sortir, borderwidth=2, relief='groove')
+frame_img.pack(side='top')
 
-butt03 = Button(frame_left, text='Export OBJ...', font=('helvetica', 14), cursor='arrow', justify='center', state='disabled', command=SaveAsOBJ)
-butt03.pack(side='top', padx=4, pady=2, fill='both')
+zanyato = Label(
+    frame_img,
+    text='Preview area.\n  Double click to open image,\n  Right click or Alt+F for a menu.\nWith image opened,\n  Ctrl+Click to zoom in,\n  Alt+Click to zoom out.',
+    font=('helvetica', 12),
+    justify='left',
+    borderwidth=2,
+    padx=24,
+    pady=24,
+    relief='groove',
+    background='grey90',
+    cursor='arrow',
+)
+zanyato.bind('<Double-Button-1>', GetSource)
+zanyato.pack(side='top', padx=0, pady=(0, 2))
 
-butt04 = Button(frame_left, text='Export STL...', font=('helvetica', 14), cursor='arrow', justify='center', state='disabled', command=SaveAsSTL)
-butt04.pack(side='top', padx=4, pady=2, fill='both')
-
-butt05 = Button(frame_left, text='Export DXF...', font=('helvetica', 14), cursor='arrow', justify='center', state='disabled', command=SaveAsDXF)
-butt05.pack(side='top', padx=4, pady=2, fill='both')
-
-butt99 = Button(frame_left, text='Exit', font=('helvetica', 14), cursor='hand2', justify='center', command=DisMiss)
-butt99.pack(side='bottom', padx=4, pady=[24, 4], fill='both')
-
-zanyato = Label(frame_right, text='Preview area'.center(16, ' '), font=('helvetica', 16), justify='center', borderwidth=2, relief='groove', state='disabled')
-zanyato.bind('<Button-1>', GetSource)
-zanyato.bind('<Button-2>', GetSource)
-zanyato.bind('<Button-3>', GetSource)
-zanyato.pack(side='top')
-
-frame_zoom = Frame(frame_right, width=300, borderwidth=2, relief='groove')
+frame_zoom = Frame(frame_img, width=300, borderwidth=2, relief='groove')
 frame_zoom.pack(side='bottom')
 
-butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomIn)
+butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', borderwidth=1, command=zoomIn)
 butt_plus.pack(side='left', padx=0, pady=0, fill='both')
 
-butt_minus = Button(frame_zoom, text='-', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomOut)
+butt_minus = Button(frame_zoom, text='-', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', borderwidth=1, command=zoomOut)
 butt_minus.pack(side='right', padx=0, pady=0, fill='both')
 
 label_zoom = Label(frame_zoom, text='Zoom 1:1', font=('courier', 8), state='disabled')
